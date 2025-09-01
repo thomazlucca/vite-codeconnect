b323901939516d69f0472c3ecff90bc9370b9f4e
Thomaz Lucca <thomazlucca@gmail.com>
2025-09-01 15:57:03 -0300

    curso alura react

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import BarraDePesquisa from "./components/BarraDePesquisa";
+import Card from "./components/Card";
+import Filtro from "./components/Filtro";
+import Ordenacao from "./components/Ordenacao";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => setDados(dados));
+  }, []);
+
+  return (
+    <div className="container">
+      <Sidebar />;
+      <div>
+        <BarraDePesquisa />;
+        <Filtro />
+        <Ordenacao />
+        <ul className="lista-cards">
+          {dados
+            ? dados.map((item, index) => (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    imagemUrl={item.imagem_capa}
+                    titulo={item.titulo}
+                    resumo={item.resumo}
+                    linhasDeCodigo={item.linhas_de_codigo}
+                    compartilhamentos={item.compartilhamentos}
+                    comentarios={item.comentarios}
+                    usuario={item.usuario}
+                  />
+                </li>
+              ))
+            : null}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
